3d3b1af6f2ad05c4332c201869b998df692080de
5s-mini <dnclsehd123@naver.com>
2025-10-16 12:23:23 +0900

    feat(application): Add input/output prompt and read logic

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str_input = Console.readLine();
+        int result = add(str_input);
+        System.out.println("결과 : " + result);
+    }
+
+    private static int add(String str_input) {
+        return 0;
     }
 }
