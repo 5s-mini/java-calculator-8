eefb36fe0d1709d9758a2e5fcb8afdeaedcdf909
5s-mini <dnclsehd123@naver.com>
2025-10-17 15:58:50 +0900

    [200~fix(calculator): throw IllegalArgumentException on invalid tokens

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,20 +1,43 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String str_input = Console.readLine();
-        int result = add(str_input);
+        String strInput = Console.readLine();
+        int result = calcSum(strInput);
         System.out.println("결과 : " + result);
     }
 
-    private static int add(String str_input) {
-        if (str_input == null || str_input.isEmpty()) {
+    private static int calcSum(String strInput) {
+        if (strInput == null || strInput.isEmpty()) {
             return 0;
         }
 
+        String normalized = strInput.replace("\\n", "\n");
+        String[] tokens = splitToTokens(normalized);
+        int sum = 0;
+
+        for (String t : tokens) {
+            if (t == null) {
+                throw new IllegalArgumentException("빈 숫자 토큰이 존재합니다.");
+            }
+
+            int value = getValue(t);
+
+            sum += value;
+        }
+
+        return sum;
+    }
+
+    private static int getValue(String t) {
+
+    }
+
+    private static String[] splitToTokens(String input) {
 
     }
 }
